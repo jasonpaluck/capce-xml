4db971ae3158b84b2c0df408c17848dfd576be8c
Jason N. Paluck <jason@paluck.com>
2023-01-10 14:26:06 -0500

    Adds registration link for first time user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :uploads
-  devise_for :users#, only: []
+  # devise_for :users #, only: []
+  devise_for :users, :controllers => { registrations: 'users/registrations' }
   resources :users
   root to: 'uploads#index'
 end
