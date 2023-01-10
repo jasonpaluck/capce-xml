176728313f19a1c731dc89e33a3123b1b6b5469b
Jason N. Paluck <jason@paluck.com>
2023-01-10 14:52:18 -0500

    Disable new user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable
   validates :firstname, :lastname, :email, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-'.]+@[a-z\d\-.]+\.[a-z]+\z/i
